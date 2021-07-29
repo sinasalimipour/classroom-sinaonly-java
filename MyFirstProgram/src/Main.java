87c5ccf43710cf221d84d8ac45821be7cfdafa43
sinagun <sinagunps4@hotmail.com>
2021-07-29 09:10:48 +0430

    0.1

---
+++ MyFirstProgram/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+	
+		//TODO print and println and \n
+		
+		System.out.println(" \ti love pizza\n");
+		System.out.print(" it reall good");
+
+	}
+
+}
