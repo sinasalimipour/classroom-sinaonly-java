69e715627fda7532c7017b3b4a3b88f8a58b4b9b
sinagun <sinagunps4@hotmail.com>
2021-07-29 10:38:24 +0430

    0.4

--- MyFirstProgram/classroom/src/classroom/Main.java
+++ MyFirstProgram/classroom/src/classroom/Main.java
@@ -1,16 +1,49 @@
 package classroom;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//variables 101
+		//TODO variables 101
 		
 		//int a;//deckaration	
 		//a=5;// assignment
 		
-		int a=123;//initialization
-		System.out.println("x");
-		System.out.println("my number is:"+a);
+//		int a=123;//initialization
+//		System.out.println("x");
+//		System.out.println("my number is: "+a);
+		
+		//String name = "bro";
+		
+		//TODO swap two variables 102
+		
+//		String x = "water";
+//		String y = "kool-aid";
+//		String tamp ;
+//		
+//		tamp=x;
+//		x=y;
+//		y=tamp;
+//		
+//		
+//		System.out.println("x: "+x);
+//		System.out.println("y: "+y);
+		
+		//TODO user input 103
+		
+		
+		
+		Scanner scanner =new Scanner(System.in);
+		
+		System.out.println("what is your name");
+		String name = scanner.nextLine();
+		System.out.println("how old are you");
+		int age = scanner.nextInt();
+		
+		
+		System.out.println("hello "+ name);
+		System.out.println("you are "+ age + "years old");
+		
 	}
 
 }
