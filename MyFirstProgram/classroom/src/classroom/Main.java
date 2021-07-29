a9b5767ef43677f3398518c1785f261a13b3cd42
sinagun <sinagunps4@hotmail.com>
2021-07-29 10:22:26 +0430

    0.2

---
+++ MyFirstProgram/classroom/src/classroom/Main.java
@@ -0,0 +1,16 @@
+package classroom;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//variables 101
+		
+		//int a;//deckaration	
+		//a=5;// assignment
+		
+		int a=123;//initialization
+		System.out.println("x");
+		System.out.println("my number is:"+a);
+	}
+
+}
